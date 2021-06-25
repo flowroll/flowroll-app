57e2ace7cebbceced5b8c6d5f908c64e924b85c2
nathan-leong-2 <62972647+nathan-leong-2@users.noreply.github.com>
2021-06-25 23:23:05 +1000

    create modal form

--- flowroll-frontend/src/components/Main.js
+++ flowroll-frontend/src/components/Main.js
@@ -1,15 +1,33 @@
 import AddBoxIcon from '@material-ui/icons/AddBox';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+import ModalAddFlow from './ModalAddFlow';
 import './Main.scss';
 
 function Main() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [filters, setFilters] = useState({
+    inflow: false,
+    outflow: false
+  });
+
+  const toggleFilter = (filterName) => {
+    setFilters({
+      ...filters,
+      [filterName]: !filters[filterName]
+    })
+  }
+
   return (
     <div className="main">
       <h1 className="flows-title">Flows</h1>
-      <span className="add-box-icon"><AddBoxIcon/></span>
+      <span className="add-box-icon" onClick={()=> setModalOpen(!modalOpen)}><AddBoxIcon/></span>
+      <ModalAddFlow show={modalOpen} onHide={() => setModalOpen(false)}/>
       <div className="filter-block">
         <span>Filters:</span>
-        <button>Inflow</button>
-        <button>Outflow</button>
+        <Button size="sm" variant="outline-primary" className="ml-2" active={filters.inflow} onClick={() => toggleFilter('inflow')}>Inflow</Button>
+        <Button size="sm" variant="outline-primary" className="ml-2" active={filters.outflow} onClick={() => toggleFilter('outflow')}>Outflow</Button>
       </div>
     </div>
   );
--- flowroll-frontend/src/components/ModalAddFlow.js
+++ flowroll-frontend/src/components/ModalAddFlow.js
@@ -1,15 +1,79 @@
-import { Modal } from '@material-ui/core';
+import { Component } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
 
-function ModalAddFlow(){
-//https://material-ui.com/components/modal/
-  return (
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="simple-modal-title"
-      aria-describedby="simple-modal-description"
-    >
-      {body}
+import NETWORK_ADDRESSES from '../networkAddresses.json';
+
+class ModalAddFlow extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      flowName: '',
+      dstAddress: '',
+      flowEndDate: null,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const formData = {
+      flowName: this.state.flowName,
+      dstAddress: this.state.dstAddress,
+      flowEndDate: this.state.flowEndDate
+    }
+    console.log(formData)
+
+    //make contract call here
+
+
+    //if successful close
+    this.props.onHide();
+  };
+
+  handleFormChange = (event) => {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
+  render(){
+    return (
+      <Modal
+        {...this.props}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">Create Flow</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form 
+        onSubmit={this.handleSubmit}>
+          <Form.Group controlId="formFlowName">
+            <Form.Label>Flow name</Form.Label>
+            <Form.Control placeholder="Aave Savings Maximiser" name="flowName" onChange={(e)=> this.handleFormChange(e)}/>
+          </Form.Group>
+
+          <Form.Group controlId="formFlowDestAddress">
+            <Form.Label>Destination Address</Form.Label>
+            <Form.Control placeholder="0x3905A1CfAe9d84fC25DffEF042f10f07Be9A7d06" name="dstAddress" onChange={(e)=> this.handleFormChange(e)}/>
+          </Form.Group>
+
+          <Form.Group controlId="formFlowEndDate">
+            <Form.Label>Terminate Flow Date</Form.Label>
+            <Form.Check type="date" name="flowEndDate" onChange={(e)=> this.handleFormChange(e)}/>
+          </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Create
+          </Button>
+        </Form>
+      </Modal.Body>
     </Modal>
   )
+    }
 }
+
+export default ModalAddFlow;
